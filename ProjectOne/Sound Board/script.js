d947d4eaad98194d68b0a6c84f06dba7a52867a5
Nitin <nitingupta2298@gmail.com>
2023-08-08 10:31:23 +0530

    Executing projects

--- ProjectOne/Sound Board/script.js
+++ ProjectOne/Sound Board/script.js
@@ -1,11 +1,25 @@
 const sounds = ['hip-hop', 'kam-riser', 'staccato', 'islandy', 'fight', 'trap', 'loop']
 
-sounds.forEach(sound =>{
+sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
 
     btn.innerText = sound
 
-    document.getElementById('buttons').
-    appendChild('btn')
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
 })
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
