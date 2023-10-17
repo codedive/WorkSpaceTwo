710bc0c37dd9cb38226cf17a893126b64db131b4
nitin gupta <nitingupta2298@gmail.com>
2023-10-17 17:26:50 +0530

    end of section 2

---
+++ Project-Four/Datingapp/API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]  //https://localhost:5001/api/users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+   {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Appuser>>> GetUsers()
+    {
+      var users = await _context.Users.ToListAsync();
+
+      return users;
+    }
+
+    [HttpGet("{id}")]  // /api/users/2
+    public async Task<ActionResult<Appuser>> GetUser(int id)
+    {
+      return await _context.Users.FindAsync(id);
+    }
+}
