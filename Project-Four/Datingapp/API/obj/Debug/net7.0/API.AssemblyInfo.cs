0f7dc28458e92d602c2baa1f6ed1f7e18e9681dc
nitin gupta <nitingupta2298@gmail.com>
2024-02-09 23:34:28 +0530

    updated

--- Project-Four/Datingapp/API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ Project-Four/Datingapp/API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6e65666b2c87be24b95c1b939238067e9d2efd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
