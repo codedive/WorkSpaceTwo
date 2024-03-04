26bb64c86b21684da7c3b22905506f7cf20ea45a
nitin gupta <nitingupta2298@gmail.com>
2024-03-05 05:22:37 +0530

    expenses tracker

--- Flutter_projects/adv_basics/lib/main.dart
+++ Flutter_projects/adv_basics/lib/main.dart
@@ -1,22 +1,6 @@
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 107, 65, 180),
-                Color.fromARGB(255, 107, 15, 168)
-              ],
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
