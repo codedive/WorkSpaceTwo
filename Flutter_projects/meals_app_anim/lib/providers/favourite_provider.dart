9a785f6c76c7836f2e5b57f84064f77b1683c875
nitin gupta <nitingupta2298@gmail.com>
2024-03-21 17:24:20 +0530

    compeleted

---
+++ Flutter_projects/meals_app_anim/lib/providers/favourite_provider.dart
@@ -0,0 +1,24 @@
+//import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+
+class FavouriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavouriteMealsNotifier() : super([]);
+
+  bool toggleMealFavouriteStatus(Meal meal) {
+    final mealIsFavouite = state.contains(meal);
+
+    if (mealIsFavouite) {
+      state = state.where((m) => m.id != meal.id).toList();
+      return false;
+    } else {
+      state = [...state, meal];
+      return true;
+    }
+  }
+}
+
+final favouriteMealProvider =
+    StateNotifierProvider<FavouriteMealsNotifier, List<Meal>>((ref) {
+  return FavouriteMealsNotifier();
+});
---
+++ Flutter_projects/meals_app_anim/lib/providers/filters_provider.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/providers/meals_provider.dart';
+//import 'package:meals/screens/filters.dart';
+
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
+class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
+  FiltersNotifier()
+      : super({
+          Filter.glutenFree: false,
+          Filter.lactoseFree: false,
+          Filter.vegetarian: false,
+          Filter.vegan: false,
+        });
+
+  void setFilters(Map<Filter, bool> choosenFilters) {
+    state = choosenFilters;
+  }
+
+  void setFilter(Filter filter, bool isActive) {
+    // state[filter] = isActive; //not allowed! => mutating state
+    state = {
+      ...state,
+      filter: isActive,
+    };
+  }
+}
+
+final filtersProvider =
+    StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
+  (ref) => FiltersNotifier(),
+);
+
+final filterMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilter = ref.watch(filtersProvider);
+  return meals.where((meal) {
+      if (activeFilter[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (activeFilter[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (activeFilter[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (activeFilter[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    }).toList();
+});
